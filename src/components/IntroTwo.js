f71a2ffb8b1b3f343f9bcba6487db9631dd27207
Michelle Morin <morin2m@gmail.com>
2020-05-02 18:17:14 -0700

    add second part of about me section

---
+++ src/components/IntroTwo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function IntroTwo() {
+  return (
+    <p>I enjoy learning new skills and sharing knowledge with others. When not coding, I'm likely trail running, cycling, or hiking. If you have any questions or want to chat, please <a href="mailto:morin2m@gmail.com">contact me</a>.</p>
+  );
+}
+
+export default IntroTwo;
