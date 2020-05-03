abe6812e35434b35070ebb51e52e2b769f35b5ea
Michelle Morin <morin2m@gmail.com>
2020-05-02 19:45:55 -0700

    change class to className

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Splash() {
   return (
     <div id="splash">
-      <div class="name-box">
+      <div className="name-box">
         <h1>Michelle Morin</h1>
         <h2>Full-Stack Software Engineer</h2>
       </div>
