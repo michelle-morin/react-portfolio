a8e22d59e1d99ee09e36dd8ca3add99085dc1393
Michelle Morin <morin2m@gmail.com>
2020-05-02 19:39:51 -0700

    add action creators and action constants

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,3 @@
+export const SHOW_SPLASH = 'SHOW_SPLASH';
+export const SHOW_PROJECTS = 'SHOW_PROJECTS';
+export const SHOW_BIO = 'SHOW_BIO';
---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import * as c from './ActionTypes';
+
+export const splash = () => ({
+  type: c.SHOW_SPLASH
+});
+
+export const projects = () => ({
+  type: c.SHOW_PROJECTS
+});
+
+export const bio = () => ({
+  type: c.SHOW_BIO
+});
