d3613f47229faf9f3bdf5aacea5bbaace423331f
Michelle Morin <morin2m@gmail.com>
2020-05-02 18:50:32 -0700

    update return

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from './Header';
+import ViewControl from './ViewControl';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <React.Fragment>
-      <Header />
-    </React.Fragment>
+    <ViewControl />
   );
 }
 
