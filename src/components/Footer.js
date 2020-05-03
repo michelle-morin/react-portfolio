f0698dc7eeefece490daa5ed6067beba3f100230
Michelle Morin <morin2m@gmail.com>
2020-05-02 18:01:28 -0700

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <footer>
+      <p>&copy; Michelle Morin 2020</p>
+      <p>All Rights Reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
