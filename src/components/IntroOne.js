bedf6247a0ff2ea33135c017816b943483f8963b
Michelle Morin <morin2m@gmail.com>
2020-05-02 18:16:59 -0700

    add first part of about me section

---
+++ src/components/IntroOne.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function IntroOne() {
+  return(
+    <React.Fragment>
+      <h3>Hello!</h3>
+      <p>I'm Michelle, a full-stack software engineer living in Portland, Oregon. I'm currently seeking full-time opportunities to build beautiful, responsive applications, collaborate with other developers, and expand my skillset.</p>
+    </React.Fragment>
+  );
+}
+
+export default IntroOne;
