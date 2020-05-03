b0432200302e296aafc74d9d760a853cfafefef1
Michelle Morin <morin2m@gmail.com>
2020-05-02 18:55:06 -0700

    import header, bio, splash, and projectlist components

---
+++ src/components/ViewControl.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Splash from './Splash';
+import Bio from './Bio';
+import ProjectList from './ProjectList';
+
+function ViewControl() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default ViewControl;
